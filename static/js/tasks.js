8938c0c7c4774017e92a4a953d0e5896da9d2795
AdrianHispania <atorres@hispaniarb.com>
2018-02-05 17:54:22 +0100

    No sé
    -Arreglado borrar registro.
    -cambios estéticos varios, iconos.

--- static/js/tasks.js
+++ static/js/tasks.js
@@ -1,5 +1,4 @@
-function deleteTask(id, elemento) {
-    document.getElementById(id).style.color = "red";
+function deleteTask(elemento) {
     var req = new XMLHttpRequest()
     req.onreadystatechange = function(){
         if (req.readyState == 4){
@@ -7,7 +6,9 @@ function deleteTask(id, elemento) {
                 //error handling code here
             }
             else{
-                 elemento.parentElement.removeChild(elemento);// se borra del dom si se ha borrado del servidor
+				var fila = document.getElementById(elemento.parentElement.parentElement.id);
+				fila.parentNode.removeChild(fila);
+				 
                 var response = JSON.parse(req.responseText)
                 document.getElementById("tasks").innerHTML = response.tasks
             }
@@ -15,7 +16,7 @@ function deleteTask(id, elemento) {
     }
     req.open('POST', '/ajax')
     req.setRequestHeader("Content-type", "application/x-www-form-urlencoded")
-    var postVars = "task="+document.getElementById(id).cells[0].innerHTML
+    var postVars = "task="+elemento.parentElement.parentElement.cells[0].innerHTML
     req.send(postVars)
     return false
 }
